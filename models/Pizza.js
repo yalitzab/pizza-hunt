54bc8d351e634fe071ced8f4bc1edd90bba61b95
yalitzab <yalitzabueno@gmail.com>
2021-02-27 20:58:06 -0800

    add commentCount and import Comment.js to index.js

--- models/Pizza.js
+++ models/Pizza.js
@@ -16,8 +16,27 @@ const PizzaSchema = new Schema({
         type: String,
         default: 'Large'
     },
-    toppings: []
-});
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.lenght;
+})
+
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
